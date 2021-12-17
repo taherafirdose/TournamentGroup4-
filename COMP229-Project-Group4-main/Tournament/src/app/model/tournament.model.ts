09a4dceb159d2967273fe3a865c87321581cdc3f
Alina <tahera.firdose@gmail.com>
2021-12-17 16:12:25 -0500

    frontend

---
+++ COMP229-Project-Group4-main/Tournament/src/app/model/tournament.model.ts
@@ -0,0 +1,9 @@
+export class Tournament{
+    constructor(
+        public _id?:number,
+        public tour?:String,
+        public game?:String,
+        public date?:String,
+        public num?:String
+    ){}
+}
---
+++ COMP229-Project-Group4-main/Tournament/src/app/model/user.model.ts
@@ -0,0 +1,8 @@
+export class User
+{
+  _id!: number;
+  username!: string;
+  password!: string;
+  email!: string;
+  displayName!: string;
+}
